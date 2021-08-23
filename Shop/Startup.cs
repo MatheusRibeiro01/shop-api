69550ec7d6a7b307dfa2dc2f252ce802896311ea
Matheus Ribeiro <matheusgomes612@gmail.com>
2021-08-23 19:48:02 -0300

    first commit

---
+++ Shop/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Shop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddScoped<DataContext, DataContext>();
+
+
+
+            //AddScopped: Garante que eu só vou ter um datacontext por requisição. Toda vez que a requisição acaba, o addscopped destroi o datacontext que destroi a conexão com o banco
+            //AddTransient: Toda vez que eu pedir um datacontext, ele vai me dar um datacontext novo.
+            //AddSingleton: Cria uma instancia de datacontext no momento em que a aplicação iniciar. Não é indicado nesse caso pois usuários podem ver sessões abertas de outros usuarios no sql server. Também pode ocorrer de a conexão com o banco ficar aberta por muito tempo.
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
