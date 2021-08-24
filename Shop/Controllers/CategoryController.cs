9f3d5bb21d66d631cdb38d2a915947532079d563
Matheus Ribeiro <matheusgomes612@gmail.com>
2021-08-24 18:03:11 -0300

    Ajustes dos metodos da controller para uso de banco em memória

--- Shop/Controllers/CategoryController.cs
+++ Shop/Controllers/CategoryController.cs
@@ -1,16 +1,21 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Shop.Models;
+
 
 [Route("categories")]
 public class CategoryController : ControllerBase
 {
     [HttpGet]
     [Route("")]
-    public async Task<ActionResult<List<Category>>> Get()
+    public async Task<ActionResult<List<Category>>> Get(
+        [FromServices] DataContext context
+    )
     {
-        return new List<Category>();
+        var categories = await context.Categories.AsNoTracking().ToArrayAsync();
+        return Ok(categories);
     }
 
     [HttpGet]
@@ -22,18 +27,32 @@ public class CategoryController : ControllerBase
     
     [HttpPost]
     [Route("")]
-    public async Task<ActionResult<List<Category>>> Post([FromBody]Category model) 
+    public async Task<ActionResult<List<Category>>> Post(
+        [FromBody]Category model,
+        [FromServices] DataContext context
+    ) 
     {
         if (!ModelState.IsValid)
-        {
             return BadRequest(ModelState);
+
+        try
+        {
+            context.Categories.Add(model);
+            await context.SaveChangesAsync();
+            return Ok(model);
         }
-        return Ok(model);
+        catch (System.Exception)
+        {
+            return BadRequest(new {message = "Não foi possível criar a categoria"});
+        }    
     }
     
     [HttpPut]
     [Route("{id:int}")]
-    public async Task<ActionResult<List<Category>>> Put(int id,[FromBody] Category model)
+    public async Task<ActionResult<List<Category>>> Put(
+        int id,
+        [FromBody] Category model,
+        [FromServices] DataContext context)
     {
         //Verifica se o Id é o mesmo da model
         if (id != model.Id) 
@@ -42,14 +61,42 @@ public class CategoryController : ControllerBase
         //Verifica se os dados são válidos
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
-          
-        return NotFound();
+
+            try
+            {
+                context.Entry<Category>(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest(new {message = "Esse registro já foi atualizado"});
+            }
+            catch (System.Exception)
+            {
+                return BadRequest(new {message = "Não foi possível atualizar esse registro"});
+            }
     }
      
     [HttpDelete]
     [Route("{id:int}")]
-    public async Task<ActionResult<List<Category>>> Delete()
+    public async Task<ActionResult<List<Category>>> Delete(
+        int id,
+        [FromServices] DataContext context)
     {
-        return Ok();
+        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id== id);
+            if (category == null)
+                return NotFound(new {message = "Categoria não encontrada"});
+
+        try
+        {
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+            return Ok(new {message = "Categoria removida com sucesso"});
+        }
+        catch (System.Exception)
+        {
+            return BadRequest(new {message = "Não foi possível remover a categoria"});
+        }        
     }
 }
